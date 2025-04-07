5ad3ffa0d58fb4cd9d1047d33358ef41dcff4d12
Grigory Yurginis <g.urginis@anviovr.com>
2025-04-07 23:24:55 +0300

    add user model

--- mentorship/admin.py
+++ mentorship/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Доп. поля', {'fields': ('phone', 'mentor')}),
+    )
+    list_display = ('username', 'email', 'phone', 'is_staff', 'is_mentor')
+    search_fields = ('username', 'email', 'phone')
--- mentorship/models.py
+++ mentorship/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Username and password are required, other fields are optional by AbstractUser
+    """
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    mentor = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        related_name='mentees',
+        on_delete=models.SET_NULL
+    )
+
+    @property
+    def is_mentor(self):
+        return self.mentees.exists()
+
+    def __str__(self):
+        return self.username
