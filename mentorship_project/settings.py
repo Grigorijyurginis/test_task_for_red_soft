c442566c5402b338f988e4746b08a63fba886249
Grigory Yurginis <g.urginis@anviovr.com>
2025-04-08 14:50:58 +0300

    return debug True

--- mentorship_project/settings.py
+++ mentorship_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x%bqjnekouq4!bp&xb8l3d&)l)_+ly7q25ewvh-ti&t)g=go^6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
